4698539f19322e0b8a3609610624fe83a30ade06
honorine22 <higiranez@gmail.com>
2022-06-30 20:04:22 +0200

    Understood seat_no function

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "bluesky.cpp"
+using namespace std;
+
+int main()
+{
+	cout << "\n:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::" << endl;
+	cout << "\t     WELCOME TO AIRLINE RESERVATION SYSTEM \n";
+	cout << ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::" << endl;
+	cout << " ..................... MENU OPTIONS .............................\n"
+		 << endl;
+
+	menu();		 // calling the menu function.
+	seat_fill(); // initialize seat array.
+
+	return 0;
+}
